97c8d6d81b71911457ac1be2be693cd67a385f7c
dblevins <dblevins@ad359b83-b104-0410-b8c9-b39b089e6ab5>
2006-03-02 03:07:51 +0000

    Test case for LinkFilterInputStream

---
+++ swizzle/src/test/java/org/codehaus/swizzle/LinkFilterInputStreamTest.java
@@ -0,0 +1,69 @@
+package org.codehaus.swizzle;
+/**
+ * @version $Revision$ $Date$
+ */
+
+import junit.framework.*;
+import org.codehaus.swizzle.LinkFilterInputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class LinkFilterInputStreamTest extends TestCase {
+    LinkFilterInputStream linkFilterInputStream;
+
+    public void testLinkFilterInputStream1() throws Exception {
+        URL url = new URL("http://swizzle.codehaus.org/stuff/");
+
+        String original = "<tr><td align=\"left\">\n<a href=\"/section1/orange.html\"><img src=\"/images/icon.gif\"\n border=\"0\"> Link Text</a></td>";
+        String exptected = "<tr><td align=\"left\">\n<a href=\"http://swizzle.codehaus.org/section1/orange.html\"><img src=\"http://swizzle.codehaus.org/images/icon.gif\"\n border=\"0\"> Link Text</a></td>";
+
+        String actual = resolveURLs(original, url);
+
+        assertEquals(exptected,actual);
+    }
+
+    public void testLinkFilterInputStream2() throws Exception {
+        URL url = new URL("http://swizzle.codehaus.org/stuff/");
+
+        String original = "<td align=\"left\"><a href=\"apple.html\"><img src=\"images/picture.gif\" border=\"0\"> Link Text</a></td>";
+        String exptected = "<td align=\"left\"><a href=\"http://swizzle.codehaus.org/stuff/apple.html\"><img src=\"http://swizzle.codehaus.org/stuff/images/picture.gif\" border=\"0\"> Link Text</a></td>";
+
+        String actual = resolveURLs(original, url);
+
+        assertEquals(exptected,actual);
+    }
+
+    public void testLinkFilterInputStream3() throws Exception {
+        URL url = new URL("http://swizzle.codehaus.org/stuff/");
+
+        String original = "<td align=\"left\">\n<a href=\"subsection/mango.html\"><img src=\"http://superimages.org/images/mango.jpg\" border=\"0\"> Link Text</a>\n</td>";
+        String exptected = "<td align=\"left\">\n<a href=\"http://swizzle.codehaus.org/stuff/subsection/mango.html\"><img src=\"http://superimages.org/images/mango.jpg\" border=\"0\"> Link Text</a>\n</td>";
+
+        String actual = resolveURLs(original, url);
+
+        assertEquals(exptected,actual);
+    }
+
+
+
+    private String resolveURLs(String original, URL url) throws IOException {
+        InputStream in = new ByteArrayInputStream(original.getBytes());
+        in = new LinkFilterInputStream(in, "<A HREF=", ">", url);
+        in = new LinkFilterInputStream(in, "SRC=\"", "\"", url);
+
+        StringBuffer text = new StringBuffer();
+        try {
+            int b;
+            while ((b = in.read()) != -1) {
+                text.append((char) b);
+            }
+        } finally {
+            in.close();
+        }
+        return text.toString();
+    }
+}
